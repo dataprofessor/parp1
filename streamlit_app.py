e51a3e48c9b1bf765546f630e6644ed25db92957
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-29 20:12:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -79,18 +79,16 @@ if st.session_state.smiles_input != '':
   
 
 # Load descriptor subset used in trained model
-#pubchem = pd.read_csv('data/PubChem_removed_low_variance_0.1_2class_chemblID.csv')
-#pubchem_subset = pubchem.drop(['molecule_chembl_id', 'class'], axis=1)
+if st.session_state.smiles_input != '':
+  model = pickle.load(open('data/oversampling_PubChem_RandomForestClassifier.pkl', 'rb'))
+  pubchem_subset = model.feature_names_in_
 
-model = pickle.load(open('data/oversampling_PubChem_RandomForestClassifier.pkl', 'rb'))
-pubchem_subset = model.feature_names_in_
+  query_desc_1 = descriptors.columns.difference(pubchem_subset)
+  #query_desc_2 = descriptors.columns.difference(query_desc_1)
+  query_desc_2 = descriptors.drop(query_desc_1, axis=1)
 
-query_desc_1 = descriptors.columns.difference(pubchem_subset)
-#query_desc_2 = descriptors.columns.difference(query_desc_1)
-query_desc_2 = descriptors.drop(query_desc_1, axis=1)
-
-st.write('**Subset of descriptor (used in trained model)**')
-st.write(query_desc_2)
+  st.write('**Subset of descriptor (used in trained model)**')
+  st.write(query_desc_2)
 
 # Read in saved classification model
 st.subheader('🤖 Predictions')
