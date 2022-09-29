75bdc12d710eb087569782688015a028a7ba91f1
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-29 19:24:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,6 +80,5 @@ if st.session_state.smiles_input != '':
 # Read in saved classification model
 if st.session_state.smiles_input != '':
   model = pickle.load(open('data/oversampling_PubChem_RandomForestClassifier.pkl', 'rb'))
-  importances = model.feature_importances_
-  model_importances = pd.Series(importances, index=feature_names)
-  st.write(model_importances)
+  importances = pd.Series(model.feature_importances_)
+  st.write(importances)
