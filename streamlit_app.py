83030e456c9919459400b53016d9404f1fd430c9
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-09-30 21:32:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,7 +63,7 @@ else:
   Chem.Draw.MolToFile(smi, 'molecule.png')
   mol_image = Image.open('molecule.png')
   
-  col1, col2, col3 = st.columns(3)
+  col1, col2, col3 = st.columns(1,3,1)
   with col1:
     st.write(' ')
   with col2:
